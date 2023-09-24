83644eeab71f07d0cb1540f96dd9d466623acef5
Leonardo Diaz <jldiaz9623@gmail.com>
2023-09-24 18:05:03 -0500

    Integración tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#EE6C4D",
+        secondary: {
+          100: "#1E1F25",
+          900: "#131517",
+        }
+      }
+    },
   },
   plugins: [],
 }
-
