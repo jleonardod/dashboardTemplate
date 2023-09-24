ea5497a8e9831da8b3f906d3b3b4f4ea40462ff4
Leonardo Diaz <jldiaz9623@gmail.com>
2023-09-24 18:46:50 -0500

    Finalización de paginas iniciales

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from "./pages/auth/Login"
 import Error404 from "./pages/Error404"
+import Register from "./pages/auth/Register"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
